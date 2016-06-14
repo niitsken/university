bfd1492e4a9d77dabc64756c80e647d563edf01c
Kensuke Niitsuma <indy0214@outlook.com>
2016-06-15 01:01:48 +0900

    travis CI test with kadai3.bash v3

--- kadai3.bash
+++ kadai3.bash
@@ -3,9 +3,9 @@ func() {
   echo $1
   exit 1
 }
+ans=$(./saidai.bash 4 18)
+[ $ans = "2"] || func "error test 1"
 temp=$(./saidai.bash 3 6)
 [ $temp = “3” ] || func “error test 1"
-temp2=$(./saidai.bash 4 18)
-[ $temp2 = "2"] || func "error test 1"
 echo “all passed"
 exit 0
