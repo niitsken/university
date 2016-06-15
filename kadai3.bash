4b06ee7e290275063d2d8685c84f75cc8f14d0a3
niitsken <indy0214@outlook.com>
2016-06-15 22:33:56 +0900

    Add files via upload

--- kadai3.bash
+++ kadai3.bash
@@ -3,11 +3,9 @@ func() {
   echo $1
   exit 1
 }
-
-
 ans=$(./saidai.bash 4 18)
-[ $ans = "2"] || func "error test 1"
+[ $ans="2"] || func "error test 1"
 temp=$(./saidai.bash 3 6)
-[ $temp = “3” ] || func “error test 1"
+[ $temp=“3” ] || func “error test 1"
 echo “all passed"
 exit 0
