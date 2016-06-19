44dacc900ec5d8cbcca90261f2b1122c2add7f2a
Kensuke Niitsuma <indy0214@outlook.com>
2016-06-19 14:38:45 +0900

    first python program

---
+++ fizz_buzz.py
@@ -0,0 +1,12 @@
+def fb(n):
+	if n % 15 == 0:
+		return "FizzBuzz"
+	elif n % 5 == 0:
+		return "Buzz"
+	elif n % 3 == 0:
+		return "Fizz"
+	return ""
+i = 1
+while i <= 20:
+	print i, fb(i)
+	i = i + 1
